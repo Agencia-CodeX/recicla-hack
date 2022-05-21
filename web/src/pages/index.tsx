440b1e60bd17f638048a44575a7c380274cc9dd3
MiguelMarcola <miguelmarcola@gmail.com>
2022-05-21 10:53:11 -0300

    setup app colors

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
     return (
-        <div className="h-screen w-full flex justify-center items-center bg-zinc-900">
-            <h1 className="text-3xl font-bold text-white">Hello World!</h1>
+        <div className="h-screen w-full flex justify-center items-center bg-zinc-800">
+            <h1 className="text-3xl font-bold text-green-50">Hello World!</h1>
         </div>
     )
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/components/**/*.tsx",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green-50": "#87F5BA",
+        "green-100": "#60F089",
+        "green-400": "#00BB58",
+        "green-700": "#1C613C",
+        "green-900": "#135726",
+      }
+    },
   },
   plugins: [],
 }
